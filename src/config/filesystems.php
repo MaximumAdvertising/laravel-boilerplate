2543272d33f74be6626e108f6fac18f7edb7a548
Jonas Van Assche <jonas.van.assche@maximum.com>
2019-05-05 16:17:45 +0800

    fix default filesystem option

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'testing' => [
+            'driver' => 'local',
+            'root' => storage_path('framework/testing/disks/testing'),
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
